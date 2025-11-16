7494f2a0f157a73c63d9292bff2ab740e0ff6cf6
Frederico Varandas <fredericovarandas1@gmail.com>
2025-11-16 15:14:07 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-5484315123-df372",
+  "appId": "1:300331022302:web:9e8b8ebee76538c7b58f4f",
+  "apiKey": "AIzaSyBLAWkF58Up9RyvlgwdLP9Wp-AMvMIPp9M",
+  "authDomain": "studio-5484315123-df372.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "300331022302"
+};
