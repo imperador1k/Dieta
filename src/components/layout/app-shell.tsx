88431ae8fd68312a27bca7763da2757c0b77e92a
Frederico Varandas <fredericovarandas1@gmail.com>
2025-11-16 09:23:19 +0000

    Isso, e quero copiar o header igualzinho à minha app antiga

--- src/components/layout/app-shell.tsx
+++ src/components/layout/app-shell.tsx
@@ -13,7 +13,7 @@ import {
 } from 'lucide-react';
 import { Icons } from '@/components/icons';
 import { Avatar, AvatarFallback, AvatarImage } from '../ui/avatar';
-import { Button, buttonVariants } from '../ui/button';
+import { Button } from '../ui/button';
 import { cn } from '@/lib/utils';
 
 const navItems = [
@@ -28,7 +28,7 @@ function Header() {
   const pathname = usePathname();
 
   return (
-    <header className="sticky top-0 z-40 w-full border-b bg-background/80 backdrop-blur-lg">
+    <header className="sticky top-0 z-40 w-full border-b bg-background/95 shadow-sm drop-shadow-sm">
       <div className="container flex h-16 items-center space-x-4 sm:justify-between sm:space-x-0">
         <div className="flex gap-6 md:gap-10">
           <Link href="/" className="flex items-center space-x-2">
