b8113b5686c96768fe927854bbdb4f12294ac130
Miguel Pereira Santos <145986231+imperador1k@users.noreply.github.com>
2026-01-02 08:47:30 +0000

    melhoras

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,12 @@
-import type {NextConfig} from 'next';
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
   typescript: {
     ignoreBuildErrors: true,
   },
-  eslint: {
-    ignoreDuringBuilds: true,
+  turbopack: {
+    root: __dirname,
   },
   images: {
     remotePatterns: [
